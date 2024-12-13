72a021408d0ab992b3089e7cfb6fcbc7b9510acf
Valerie <123942203+ValerieMbaka@users.noreply.github.com>
2024-12-13 11:11:31 +0300

    Delete users directory

--- users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import FirebaseUser
-
-# from .models import FirebaseUser
-
-# Register your models here.
-# admin.site.register(UserProfile)
-admin.site.register(FirebaseUser)
-
